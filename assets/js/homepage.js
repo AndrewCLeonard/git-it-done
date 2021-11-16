ac0c69813b678c38b8e04faea23b9dac0e3beec0
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-16 09:17:09 -0800

    find user functionality no longer hardcoded

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,19 @@
-var getUserRepos = function () {
-	fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+	// format the github api url
+	var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+	// make a request to the url
+	fetch(apiUrl).then(function (response) {
+		// adding response.json formats the data in the response so it's usable
+		response.json().then(function (data) {
+			console.log("inside", response);
+		});
+	});
 };
 
-getUserRepos();
+console.log("outside");
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+
+getUserRepos("microsoft");
