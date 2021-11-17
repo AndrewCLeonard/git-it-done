e96bdffbf675518f7cc51cc630c6a2e2a5a45373
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-16 19:26:25 -0800

    update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+/* 
+https://courses.bootcampspot.com/courses/951/pages/6-dot-1-5-capture-data-returned-from-the-api?module_item_id=331368
+6.1.5 Capture Data Returned from the API
+*/
+
 var getUserRepos = function () {
 	fetch("https://api.github.com/users/octocat/repos");
 };
