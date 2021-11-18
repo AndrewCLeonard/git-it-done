c9abff631e3e5b4cef430ccc3e68bc501f5e6573
Andrew Leonard <AndrewCLeonard@fastmail.com>
2021-11-17 21:35:10 -0800

    astarted adding fetch to dom

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,11 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+	event.preventDefault();
+	console.log(event);
+};
+
 var getUserRepos = function (user) {
 	// format the github api url
 	var apiUrl = "https://api.github.com/users/" + user + "/repos";
